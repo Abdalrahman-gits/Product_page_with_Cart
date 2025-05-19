9a9588fb29bd3653eed82d8b4152a4fb73840673
Abdalrahman_Gamal <ghozalabdo456@gmail.com>
2025-05-19 03:57:59 +0300

    Some Enhancements

--- main.js
+++ main.js
@@ -74,11 +74,11 @@ function addbtnHandle(event) {
     addToCart(clicked.closest(".card"));
   }
 
-  if(clicked.classList.contains("plus")) {
+  if(clicked.closest(".plus")) {
     increment(clicked.closest(".card"))
   }
   
-  if(clicked.classList.contains("minus")) {
+  if(clicked.closest(".minus")) {
     decrement(clicked.closest(".card"))
   }
 }
@@ -109,9 +109,17 @@ function addbtnAdded(product) {
   let addbtn = product.querySelector(".add-btn");
   let productClass = cartArray.find((item) => item.name === product.getAttribute("name"));
   addbtn.innerHTML = `
-  <img class="minus" src="assets/images/icon-decrement-quantity.svg">
+  <div class="minus">
+    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2">
+      <path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/>
+    </svg>
+  </div>
   <span class="quant ml-5 mr-5">${productClass.quantity}</span>
-  <img class="plus" src="assets/images/icon-increment-quantity.svg"> 
+  <div class="plus">
+    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10">
+      <path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/>
+    </svg>
+  </div> 
   `;
 }
 
@@ -161,6 +169,7 @@ function removeFromCart(product) {
 
 function updateCart() {
   if(!cartArray.length) {
+    counter.textContent = 0;
     cartEmpty();
   }
   else {
@@ -171,13 +180,17 @@ function updateCart() {
     for(let product of cartArray) {
       content += `
       <div class="product" name="${product.name}">
-       <div class="details">
-       <h5>${product.name}</h5>
-       <span class="quant">${product.quantity}x</span>
-       <span class="price">@ $${product.price}</span>
-       <span class="total-price">$${(+product.quantity * +product.price).toFixed(2)}</span>
-       </div>
-       <div class="delete-btn" name="${product.name}"><img src="assets/images/icon-remove-item.svg"></img></div>
+        <div class="details">
+          <h5>${product.name}</h5>
+          <span class="quant">${product.quantity}x</span>
+          <span class="price">@ $${product.price}</span>
+          <span class="total-price">$${(+product.quantity * +product.price).toFixed(2)}</span>
+        </div>
+        <div class="delete-btn" name="${product.name}">
+          <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10">
+            <path fill="#CAAFA7" d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z"/>
+          </svg>
+        </div>
       </div> 
       `;
       numofproducts += +product.quantity;
@@ -189,13 +202,22 @@ function updateCart() {
       <span>Order Total</span>
       <p class="bill">$${(totalPrice.toFixed(2))}</p>
     </div>
-    `
+    `;
+    let neutral = `
+    <div class="neutral">
+      <svg class="mr-5" xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20">
+        <path fill="#1EA575" d="M8 18.75H6.125V17.5H8V9.729L5.803 8.41l.644-1.072 2.196 1.318a1.256 1.256 0 0 1 .607 1.072V17.5A1.25 1.25 0 0 1 8 18.75Z"/><path fill="#1EA575" d="M14.25 18.75h-1.875a1.25 1.25 0 0 1-1.25-1.25v-6.875h3.75a2.498 2.498 0 0 0 2.488-2.747 2.594 2.594 0 0 0-2.622-2.253h-.99l-.11-.487C13.283 3.56 11.769 2.5 9.875 2.5a3.762 3.762 0 0 0-3.4 2.179l-.194.417-.54-.072A1.876 1.876 0 0 0 5.5 5a2.5 2.5 0 1 0 0 5v1.25a3.75 3.75 0 0 1 0-7.5h.05a5.019 5.019 0 0 1 4.325-2.5c2.3 0 4.182 1.236 4.845 3.125h.02a3.852 3.852 0 0 1 3.868 3.384 3.75 3.75 0 0 1-3.733 4.116h-2.5V17.5h1.875v1.25Z"/>
+      </svg>
+      <p class="txt-cap">this is a <span class="carbon">carbon-neutral</span> delivery</p>
+    </div>
+    `;
     let submit = document.createElement("input");
     submit.type = "submit";
-    submit.value = `Order Now`;
-    submit.classList.add("submit","mt-30");
+    submit.value = `Confirm Order`;
+    submit.classList.add("submit");
     cartBody.innerHTML = content;
     cartBody.innerHTML += order;
+    cartBody.innerHTML += neutral;
     cartBody.appendChild(submit);
   }
 }
