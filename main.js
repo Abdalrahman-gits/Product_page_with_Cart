81dfa343e64816ac20f7a7b183aca82934871a32
Abdalrahman_Gamal <ghozalabdo456@gmail.com>
2025-05-20 23:49:19 +0300

    Project Finished

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 let navIcon = document.querySelector(".nav-icon");
 let navMenu = document.querySelector(".nav-main");
 
+// Validation on Values
 if(navIcon && navMenu) {
   navIcon.addEventListener("click", _ => {
   navMenu.classList.toggle("show");
@@ -13,18 +14,29 @@ else {
 
 /* =============== Fetching Data =============== */
 async function getData(url) {
-  let resposne = await fetch(url,{"method":"Get"});
-  let data = await resposne.json();
-  return data;
+  try {
+    let response = await fetch(url,{"method":"Get"});
+    if(!response.ok) {
+      throw new Error("Problems in Network");
+    }
+    let data = await response.json();
+    return data;
+  }
+  catch (err) {
+    console.error("Fetching Error : ",err);
+    throw err;
+  }
 }
 
+
+// Displaying Data on Document
 function displayData(data) {
   let cardscontainer = document.querySelector(".main-page .desserts .cards")
   let cardsContent = ``; // Prevent Rewrite in document and print for one Time.
 
   for (let i = 0; i < data.length; i++) {
     cardsContent += `
-        <div class="card" name="${data[i].name}" data-price="${(data[i].price).toFixed(2)}">
+        <div class="card" name="${data[i].name}" data-price="${(data[i].price).toFixed(2)}" data-thumbnailImg="${data[i].image.thumbnail}">
           <div class="img">
             <img src="${data[i].image.tablet}" alt="">
           </div>
@@ -52,59 +64,115 @@ let desserts = document.querySelector(".desserts")
 let cartBody = document.querySelector(".Cart-body");
 let counter = document.querySelector(".counter");
 
+// Getting Data From SessionStorage
 let cartArray = sessionStorage.getItem("cartContent") ? JSON.parse(sessionStorage.getItem("cartContent")) : [];
 
+
+// Intializing Cart 
 function cartInit() {
   let cardsinstorage = document.querySelectorAll(".card");
   if(cardsinstorage) {
     cardsinstorage.forEach((card) => {
+      // If item Found in Cart => add the class Added to it & Editin Shape
       if(cartArray.some((item) => item.name === card.getAttribute("name"))) {
         card.classList.add("added");
         addbtnAdded(card);
       }
     })
   }
-  desserts.addEventListener("click",addbtnHandle);
-  cartBody.addEventListener("click",deletebtn);
+
+  // Events in Page
+  desserts.addEventListener("click",dessertEvents);
+  cartBody.addEventListener("click",cartEvents);
+  document.body.addEventListener("click",modalEvents);
 }
 
-function addbtnHandle(event) {
+
+// Events in Dessert Section
+function dessertEvents(event) {
   let clicked = event.target;
+
+  // This is add-btn But dont have the class added (Added to Cart)
   if(clicked.closest(".add-btn") && !clicked.closest(".card").classList.contains("added")) {
+    // Add Product to Cart
     addToCart(clicked.closest(".card"));
   }
 
+  // Click on inc. Button
   if(clicked.closest(".plus")) {
     increment(clicked.closest(".card"))
   }
   
+  // Click on Dec. Button
   if(clicked.closest(".minus")) {
     decrement(clicked.closest(".card"))
   }
 }
 
-function deletebtn(event) {
-  let clicked = event.target.closest(".delete-btn");
-  if(clicked) {
+
+// Events on Cart Elements
+function cartEvents(event) {
+  let clicked = event.target;
+
+  // Delete Icon
+  if(clicked.closest(".delete-btn")) {
     removeFromCart(clicked.closest(".product"))
     addbtnInit(desserts.querySelector(`[name="${clicked.closest(".product").getAttribute("name")}"]`))
     updateCart();
   }
+
+  // Confirm Button => Create Modal
+  if( clicked.closest(".confirm") ) {
+    createModal();
+  }
 }
 
+
+// Events On Modal
+function modalEvents(event) {
+  let clicked = event.target;
+  
+  // On clicking Any where on screen the modal will be removed
+  if( document.body.classList.contains("modal-opened") && clicked.closest(".modal") && !clicked.closest(".modal-content") ) {
+    removeModal();
+  }
+
+  // (New Order) Button Event => Remove modal and All Products from Cart
+  if(clicked.classList.contains("new-order")) {
+    removeModal();
+    cartArray.forEach((item) => {
+      addbtnInit(document.querySelector(`[name="${item.name}"]`));
+    });
+
+    cartArray = [];
+    window.sessionStorage.removeItem("cartContent");
+    updateCart();
+  }
+}
+
+
+// Add to Cart Function
 function addToCart(product) {
+  
+  // Adding (added) Class to Product 
   product.classList.add("added");
+
+  // Object For Each Product
   const info = {
     name: product.getAttribute("name"),
     quantity: 1,
     price: product.dataset.price,
+    thumbnailImg: product.getAttribute("data-thumbnailImg"),
   }
+
   cartArray.push(info);
   addbtnAdded(product);
   window.sessionStorage.setItem("cartContent",JSON.stringify(cartArray));
   updateCart();
 }
 
+
+// Creating Shape of (add-btn) After Adding to Cart.
 function addbtnAdded(product) {
   let addbtn = product.querySelector(".add-btn");
   let productClass = cartArray.find((item) => item.name === product.getAttribute("name"));
@@ -123,6 +191,8 @@ function addbtnAdded(product) {
   `;
 }
 
+
+// (add-btn) intital Shape (Before adding to Cart)
 function addbtnInit(product) {
   let addbtn = product.querySelector(".add-btn");
   addbtn.innerHTML = `
@@ -131,15 +201,18 @@ function addbtnInit(product) {
   product.classList.remove("added");
 }
 
+// Shape of Cart When Empty
 function cartEmpty() {
   cartBody.innerHTML = `
-        <div class="Cart-img">
-          <img src="assets/images/illustration-empty-cart.svg" alt="">
-        </div>
-        <p>Your added items will appear here</p>
+    <div class="Cart-img">
+      <img src="assets/images/illustration-empty-cart.svg" alt="">
+    </div>
+    <p>Your added items will appear here</p>
   `;
 }
 
+
+// Incrementing The Quantity of a Product.
 function increment(product) {
   let item = cartArray.find((pro) => pro.name === product.getAttribute("name"));
   item.quantity++;
@@ -148,26 +221,39 @@ function increment(product) {
   updateCart();
 }
 
+// Decrementing The Quantity of a Product.
 function decrement(product) {
   let item = cartArray.find((pro) => pro.name === product.getAttribute("name"));
+
+  // if (quantity == 1)  => remove product from cart
   if(item.quantity == 1) {
     addbtnInit(product);
     removeFromCart(product);
   }
+
+  // else => decrement
   else {
     item.quantity--;
     product.querySelector(".quant").innerHTML = item.quantity;
     window.sessionStorage.setItem("cartContent",JSON.stringify(cartArray));
   }
+
   updateCart();
 }
 
+
+
+// Removing From Cart
 function removeFromCart(product) {
   cartArray = cartArray.filter((item) => item.name !== product.getAttribute("name"));
   window.sessionStorage.setItem("cartContent",JSON.stringify(cartArray));
 }
 
+
+// Update the Cart Content
 function updateCart() {
+
+  // If Cart is Empty
   if(!cartArray.length) {
     counter.textContent = 0;
     cartEmpty();
@@ -214,12 +300,68 @@ function updateCart() {
     let submit = document.createElement("input");
     submit.type = "submit";
     submit.value = `Confirm Order`;
-    submit.classList.add("submit");
-    cartBody.innerHTML = content;
-    cartBody.innerHTML += order;
-    cartBody.innerHTML += neutral;
+    submit.classList.add("confirm","main-btn");
+    cartBody.innerHTML = content + order + neutral;
     cartBody.appendChild(submit);
   }
 }
 
+
+// Creating Modal Page in Body
+function createModal() {
+  let totalPrice = 0;
+  let modalEle = document.createElement("div");
+  let orders = ``;
+  for(const product of cartArray) {
+    orders += `
+    <div class="item d-flex">
+      <img src="${product.thumbnailImg}">
+      <div class="details">
+        <h5>${product.name}</h5>
+        <span class="quant">${product.quantity}x</span>
+        <span class="price">@ $${product.price}</span>
+      </div>
+      <span class="total-price">$${(+product.quantity * +product.price).toFixed(2)}</span>
+    </div>
+    `;
+    totalPrice += (+product.quantity * +product.price);
+  }
+  orders += `
+    <div class="order mt-25 between-align-c">
+      <span class="fs-14">Order Total</span>
+      <p class="bill fs-24 bold">$${(totalPrice.toFixed(2))}</p>
+    </div>
+  `;
+  let modal = `
+    <div class="modal-content p-20 fs-14">
+      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
+        <path d="M21 32.121L13.5 24.6195L15.6195 22.5L21 27.879L32.3775 16.5L34.5 18.6225L21 32.121Z" fill="#1EA575"/>
+        <path d="M24 3C19.8466 3 15.7865 4.23163 12.333 6.53914C8.8796 8.84665 6.18798 12.1264 4.59854 15.9636C3.0091 19.8009 2.59323 24.0233 3.40352 28.0969C4.21381 32.1705 6.21386 35.9123 9.15077 38.8492C12.0877 41.7861 15.8295 43.7862 19.9031 44.5965C23.9767 45.4068 28.1991 44.9909 32.0364 43.4015C35.8736 41.812 39.1534 39.1204 41.4609 35.667C43.7684 32.2135 45 28.1534 45 24C45 18.4305 42.7875 13.089 38.8493 9.15076C34.911 5.21249 29.5696 3 24 3ZM24 42C20.4399 42 16.9598 40.9443 13.9997 38.9665C11.0397 36.9886 8.73256 34.1774 7.37018 30.8883C6.0078 27.5992 5.65134 23.98 6.34587 20.4884C7.04041 16.9967 8.75474 13.7894 11.2721 11.2721C13.7894 8.75473 16.9967 7.0404 20.4884 6.34587C23.98 5.65133 27.5992 6.00779 30.8883 7.37017C34.1774 8.73255 36.9886 11.0397 38.9665 13.9997C40.9443 16.9598 42 20.4399 42 24C42 28.7739 40.1036 33.3523 36.7279 36.7279C33.3523 40.1036 28.7739 42 24 42Z" fill="#1EA575"/>
+      </svg>
+      <div class="order-confirmed mt-15 mb-30">
+        <h2 class="fs-22">Order Confirmed</h2>
+        <span class="fs-13">We hope you enjoy your food!</span>
+      </div>
+      <div class="order-done">
+        ${orders}
+      </div>
+      <input class="new-order main-btn mt-30" type="submit" value="Start New Order">
+    </div>
+  `;
+  modalEle.innerHTML = modal;
+  modalEle.classList.add("modal");
+
+  document.body.appendChild(modalEle);
+  document.body.classList.add("modal-opened");
+}
+
+
+// Removing Modal
+function removeModal() {
+  document.body.removeChild(document.querySelector(".modal"));
+  document.body.classList.remove("modal-opened");
+}
+
+
+// Updating Cart On Starting
 updateCart();
